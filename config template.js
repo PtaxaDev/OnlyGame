a36d56b43de722d5a67bca7326e97fcfe9918ded
Ptaxa <gigobit2001@gmail.com>
2025-12-31 22:54:25 +0200

    add config template

---
+++ config template.js
@@ -0,0 +1,34 @@
+/*
+// placeholder
+
+  {
+    image: "",
+    name: "",
+    description:
+      "",
+    // video: "",
+    link: "https://store.steampowered.com/",
+    status: "completed",
+    date: "2022-11-10",
+  },
+
+    "completed": "Пройденно",
+    "not-completed": "Не пройденно",
+    "in-progress": "В процессе",
+    "rejected": "Забракованно"
+
+*/
+
+const cardsConfig = [
+  {
+    image:
+      "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/493520/header.jpg?t=1728027032",
+    name: "GTFO",
+    description:
+      "В жестоком командном шутере GTFO вы должны сражаться как единое целое – или все поляжете в этой жуткой подземной тюрьме. А чтобы преуспеть, помните о правиле «четырех С»: скорость, скрытность, стратегия, слаженная командная работа.",
+    // video: "https://www.youtube.com/embed/VmD0uVYkz5Y",
+    link: "https://store.steampowered.com/app/493520/GTFO/",
+    status: "in-progress",
+    date: "-",
+  },
+];
